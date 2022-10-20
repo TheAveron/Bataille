946f72dc5754951ff10f12ccedf465ce8152e94f
Averon <victor.averseng@gmail.com>
2022-10-20 15:47:49 +0200

    Add Docstrings to carte.py

--- carte.py
+++ carte.py
@@ -2,6 +2,10 @@ from math import *
 
 class Carte:
   def __init__(self,couleur:str,valeur:int)-> None:
+    """
+    IN : couleur la couleur de la carte (trefle, carreau, pique, coeur), valeur la valeur de la carte (entre 1 et 14)
+    OUT: défini les caracteristiques de la carte
+    """
     if (valeur > 14 or valeur < 2):
       raise ValueError(valeur)
     elif (couleur not in ["trefle","coeur","pique","carreau"]):
@@ -11,10 +15,19 @@ class Carte:
       self.couleur=couleur
 
   def GetAttributs(self:object)-> tuple:
+    """
+    OUT: renvoie la couleur et la valeur de la carte
+    """
     return self.couleur,self.valeur
 
   def GetValeur(self:object)->int:
+    """
+    OUT: renvoie la valeur de la carte
+    """
     return self.valeur
   
   def GetCouleur(self:object)->str:
+    """
+    OUT: renvoie la couleur de la carte
+    """
     return self.couleur
